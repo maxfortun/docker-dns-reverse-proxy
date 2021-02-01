3e4e537861e5b6686af603db11f7808bc1d2f6e3
Max Fortun <max@maxf.net>
2021-01-31 21:41:25 -0500

    added

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 export BIND_ADDRESS=$(ip -4 -o addr show eth0 | awk '{print $4}' | cut -d/ -f1)
 
 cd /usr/local/bin/dns-reverse-proxy
-DEBUG=* ./dns-reverse-proxy.js
+DEBUG="*,-*:debug" ./dns-reverse-proxy.js
